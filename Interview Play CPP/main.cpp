96e2b0a21aacf7d0b15ccc35386bfba3c02ca641
Jingtao Ren <Terrence@JingtaomatoMacBook-Air.local>
2014-12-07 15:15:19 -0800

    Initial Commit

---
+++ Interview Play CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Interview Play CPP
+//
+//  Created by Jingtao Ren on 14/12/7.
+//  Copyright (c) 2014年 Jingtao Ren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
