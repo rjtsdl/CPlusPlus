b1aab67dd9444b8d1ac84b3b1038c44b134cf493
Jingtao <jingtao@jingtaos-macbook-pro.local>
2016-02-08 21:34:47 -0800

    Group Anagram and rotate image

---
+++ Interview Play CPP/GroupAnagrams.cpp
@@ -0,0 +1,55 @@
+//
+//  GroupAnagrams.cpp
+//  Interview Play CPP
+//
+//  Created by Jingtao on 2/8/16.
+//  Copyright © 2016 Jingtao Ren. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ret;
+        unordered_map<string, int> theMap;
+        
+        for (string& str : strs) {
+            string key = getTheKey(str);
+            auto iter = theMap.find(key);
+            if (iter == theMap.end()) {
+                //No such one yet;
+                theMap[key] = (int)ret.size();
+                ret.push_back(vector<string>{str});
+            }
+            else
+            {
+                int index = iter->second;
+                ret[index].push_back(str);
+            }
+        }
+        
+        for (auto& vec : ret) {
+            sort(vec.begin(), vec.end());
+        }
+        return ret;
+    }
+    
+    string getTheKey(const string str)
+    {
+        string ret = str;
+        sort(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<string> in = {""};
+    sol.groupAnagrams(in);
+}
---
+++ Interview Play CPP/RotateImage.cpp
@@ -0,0 +1,42 @@
+//
+//  RotateImage.cpp
+//  Interview Play CPP
+//
+//  Created by Jingtao on 2/8/16.
+//  Copyright © 2016 Jingtao Ren. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int len = (int)matrix.size();
+        if (len == 0) {
+            return;
+        }
+        if (len != matrix[0].size()) {
+            return;
+        }
+        
+        // Now we get a valid len;
+        int width = (int)ceil(len * 0.5);
+        int height = (int)floor(len * 0.5);
+        
+        int temp = 0;
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                temp = matrix[j][i];
+                matrix[j][i] = matrix[len-1-i][j];
+                matrix[len -i -1][j] = matrix[len - j -1][len-i-1];
+                matrix[len-j-1][len-i-1] = matrix[i][len-j-1];
+                matrix[i][len-j-1] = temp;
+            }
+        }
+        return;
+        
+    }
+};
