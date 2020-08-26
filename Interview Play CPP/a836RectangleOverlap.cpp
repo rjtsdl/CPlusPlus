632d3dfc6f35a54d5fc22b31bc9404a0645e81b5
Jingtao Ren <jingtao.ren@oracle.com>
2020-08-25 17:39:36 -0700

    1

---
+++ Interview Play CPP/a836RectangleOverlap.cpp
@@ -0,0 +1,22 @@
+//
+//  a836RectangleOverlap.cpp
+//  Interview Play CPP
+//
+//  Created by jingtao ren on 8/25/20.
+//  Copyright © 2020 Jingtao Ren. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        return !(rec1[2] <= rec2[0] ||   // left
+        rec1[3] <= rec2[1] ||   // bottom
+        rec1[0] >= rec2[2] ||   // right
+        rec1[1] >= rec2[3]);    // top
+    }
+};
